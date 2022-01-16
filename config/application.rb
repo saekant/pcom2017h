00bcc2a9bd537fce18e2d9a4a8feaeef06709e86
Saeka TAKABAYASHI <saeka1229@gmail.com>
2022-01-16 14:00:22 +0900

    comment out
    #config.serve_static_assets

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.serve_static_assets = true
+#config.serve_static_assets = true
 config.assets.initialize_on_precompile = false
 
 module Pcom202110
