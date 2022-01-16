7d189a239338d0df17e25903280cf4093562061d
Saeka TAKABAYASHI <saeka1229@gmail.com>
2022-01-16 14:03:11 +0900

    l10 comment out

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 
 #config.serve_static_assets = true
-config.assets.initialize_on_precompile = false
+#config.assets.initialize_on_precompile = false
 
 module Pcom202110
   class Application < Rails::Application
