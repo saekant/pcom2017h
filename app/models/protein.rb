15bd1165db9d4649aaa2e344229c1fc5eecd33cc
Saeka TAKABAYASHI <saeka1229@gmail.com>
2023-06-01 19:53:10 +0900

    20230601Update
    Search error fixed

--- app/models/protein.rb
+++ app/models/protein.rb
@@ -1,2 +1,31 @@
 class Protein < ActiveRecord::Base
+        def symbols_tr
+            if self.symbols!=nil
+        	     return self.symbols.tr('#',',')
+        	else
+        	    return ""
+        	end
+        end
+        def description_tr
+        	if self.description!=nil
+        	     return self.description.tr('#',',')
+        	else
+        	    return ""
+        	end
+        end
+        def arabi_symbols_tr
+         	if self.arabi_symbols!=nil
+        	     return self.arabi_symbols.tr('#',',')
+        	else
+        	    return ""
+        	end
+        end
+        def arabi_description_tr
+        	if self.arabi_description!=nil
+        	     return self.arabi_description.tr('#',',')
+        	else
+        	    return ""
+        	end
+
+        end
 end
